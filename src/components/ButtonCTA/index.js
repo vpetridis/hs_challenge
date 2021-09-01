0ea04ddeeed0c0eba13ba48420e565ff1819c9e0
vpetridis <vpetridis@modulus.gr>
2021-09-01 13:32:17 +0300

    CREATED main ButtonCTA component

---
+++ src/components/ButtonCTA/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ButtonCTA.css'
+
+export const ButtonCTA = ({text}) => {
+   return (
+      <div className="ButtonCTA">
+         <p className="ButtonCTA--text">{text}</p>
+      </div>
+   )
+}
